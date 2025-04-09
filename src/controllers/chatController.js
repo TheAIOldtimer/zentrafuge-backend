e2253b6764e7895b1217044e6049b87567f1e4a9
The AI Oldtimer <theaioldtimer@gmail.com>
2025-04-09 18:01:27 +0100

    Update chatController.js

--- src/controllers/chatController.js
+++ src/controllers/chatController.js
@@ -4,11 +4,14 @@ const openaiConfig = require('../config/openai');
 const admin = require('firebase-admin');
 
 // Initialize Firebase
-const serviceAccount = require('../../firebase-service-account.json');
 if (!admin.apps.length) {
   admin.initializeApp({
-    credential: admin.credential.cert(serviceAccount),
-    databaseURL: `https://${serviceAccount.project_id}.firebaseio.com`
+    credential: admin.credential.cert({
+      projectId: process.env.FIREBASE_PROJECT_ID,
+      clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+      privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, '\n')
+    }),
+    databaseURL: `https://${process.env.FIREBASE_PROJECT_ID}.firebaseio.com`
   }) ;
 }
 

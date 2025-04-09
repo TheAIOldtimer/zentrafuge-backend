4db5304f826e70cd00aca1c207522dd710b80626
The AI Oldtimer <theaioldtimer@gmail.com>
2025-04-09 18:02:27 +0100

    Update growthController.js

--- src/controllers/growthController.js
+++ src/controllers/growthController.js
@@ -5,13 +5,14 @@ const firebaseConfig = require('../config/firebase');
 if (!admin.apps.length) {
   admin.initializeApp({
     credential: admin.credential.cert({
-      projectId: firebaseConfig.projectId,
+      projectId: process.env.FIREBASE_PROJECT_ID,
       clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-     privateKey: process.env.FIREBASE_PRIVATE_KEY
+      privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, '\n')
     }),
-    databaseURL: `https://${firebaseConfig.projectId}.firebaseio.com`
+    databaseURL: `https://${process.env.FIREBASE_PROJECT_ID}.firebaseio.com`
   }) ;
 }
+
 
 const db = admin.firestore();
 

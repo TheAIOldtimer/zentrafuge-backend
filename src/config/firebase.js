5a8e664f63309bcb71f8a31b335950348a4d59ea
The AI Oldtimer <theaioldtimer@gmail.com>
2025-04-09 04:11:58 +0100

    Create firebase.js

---
+++ src/config/firebase.js
@@ -0,0 +1,14 @@
+// Firebase configuration file
+// Replace this with your actual Firebase configuration
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCDVzKtLrqxmb8PF3ffGNw6jrFTdvBB_po",
+  authDomain: "zentrafuge.firebaseapp.com",
+  projectId: "zentrafuge",
+  storageBucket: "zentrafuge.firebasestorage.app",
+  messagingSenderId: "12314078875",
+  appId: "1:12314078875:web:fa70fed2bba25ca4daa03e",
+  measurementId: "G-007Z072ZP7"
+};
+
+module.exports = firebaseConfig;
